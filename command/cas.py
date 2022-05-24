7e60bcd7a1f7a4740c74d777f2130decc13201a8
Aneudy Mota <aneudy.motacatalino@gmail.com>
2022-05-24 13:03:33 -0400

    Added some basic error handling

--- command/cas.py
+++ command/cas.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import sys
+
 import utils.parser
 
 
@@ -11,34 +13,47 @@ class Cas:
 
     def login(self):
         cp = subprocess.run([self.CAS_BIN, 'login'], capture_output=True, universal_newlines=True)
+        if cp.stderr:
+            sys.exit(cp.stderr)
+
 
     def notarize_test(self, asset):
         cp = subprocess.run(
             [self.CAS_BIN, 'notarize', os.path.join(self.TEST_DATA_DIR, asset), '--api-key', self.CAS_API_KEY],
             capture_output=True, universal_newlines=True)
+        if cp.stderr:
+            sys.exit(cp.stderr)
         return utils.parser.parse_stdout(cp.stdout)
 
     def notarize_file(self, asset):
         cp = subprocess.run(
             [self.CAS_BIN, 'notarize', os.path.join(self.TEST_DATA_DIR, asset), '--api-key', self.CAS_API_KEY],
             capture_output=True, universal_newlines=True)
+        if cp.stderr:
+            sys.exit(cp.stderr)
         return utils.parser.parse_stdout(cp.stdout)
 
     def notarize_repo(self, repo_path):
         cp = subprocess.run(
             [self.CAS_BIN, 'notarize', 'git://' + repo_path, '--api-key', self.CAS_API_KEY, '--output=json'],
             capture_output=True, universal_newlines=True)
+        if cp.stderr:
+            sys.exit(cp.stderr)
         return utils.parser.parse_json(cp.stdout)
 
     def notarize_docker_image(self, image_name):
         cp = subprocess.run(
             [self.CAS_BIN, 'notarize', 'docker://' + image_name, '--api-key', self.CAS_API_KEY, '--output=json'],
             capture_output=True, universal_newlines=True)
+        if cp.stderr:
+            sys.exit(cp.stderr)
         return utils.parser.parse_json(cp.stdout)
 
     def authenticate_file(self, asset):
         cp = subprocess.run(['cas', 'authenticate', asset, '--api-key', self.CAS_API_KEY],
                             capture_output=True, universal_newlines=True)
+        if cp.stderr:
+            sys.exit(cp.stderr)
         return utils.parser.parse_stdout(cp.stdout)
 
     def authenticate_repo(self):
--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import shutil
 import stat
 import subprocess
 import sys
+import time
+
 import pytest
 
 from git import Repo
@@ -18,7 +20,6 @@ def clone_repo(repo_url, target_dir):
 
 
 def make_cas():
-    print(os.getcwd())
     subprocess.run(['make', 'cas'])
 
 
@@ -28,6 +29,9 @@ if __name__ == "__main__":
     cas_repo_path = os.path.join("test_data", "cas")
     url = 'https://github.com/codenotary/cas.git'
 
+    if "CAS_API_KEY" not in os.environ:
+        sys.exit("CAS_API_KEY not set")
+
     if os.path.isdir(bin_dir):
         shutil.rmtree(bin_dir, onerror=remove_readonly)
         clone_repo(url, bin_dir)
@@ -35,7 +39,10 @@ if __name__ == "__main__":
         make_cas()
     else:
         clone_repo(url, bin_dir)
+        os.chdir(bin_dir)
+        make_cas()
+
+
 
     os.chdir(project_base_dir)
-    print(project_base_dir)
     sys.exit(pytest.main(['-s']))
