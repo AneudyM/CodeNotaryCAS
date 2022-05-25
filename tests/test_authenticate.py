9bf3053142554103bc20dc3a0f8668c73053970a
Aneudy Mota <aneudy.motacatalino@gmail.com>
2022-05-25 19:08:23 -0400

    Added selenium webdriver manager and fixed dockerfile as well as other bugs

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -1,9 +1,12 @@
 
 import os
 
+import pytest
+
 from command.cas import Cas
 
 
+@pytest.mark.order(2)
 def test_authenticate_text_file():
     test_file = os.path.join(os.getcwd(), 'test_data', 'Aneudy_Resume.txt')
     cas = Cas()
@@ -12,6 +15,7 @@ def test_authenticate_text_file():
     assert res.get('verified') is True
 
 
+@pytest.mark.order(4)
 def test_authenticate_git_repo():
     test_repo = os.path.join(os.getcwd(), 'test_data', 'working')
     cas = Cas()
--- tests/test_notarize.py
+++ tests/test_notarize.py
@@ -1,9 +1,11 @@
 import os
-
 import utils.repo
+import pytest
+
 from command.cas import Cas
 
 
+@pytest.mark.order(1)
 def test_notarize_file():
     test_file = os.path.join(os.getcwd(), 'test_data', 'Aneudy_Resume.txt')
     cas = Cas()
@@ -12,6 +14,7 @@ def test_notarize_file():
     assert res.get('Status') == "TRUSTED"
 
 
+@pytest.mark.order(3)
 def test_notarize_git_repo():
     test_repo = os.path.join(os.getcwd(), 'test_data', 'working')
     cas = Cas()
