9cf7f0d95f91e2a31a0e83c6315a1eb951762d12
Aneudy Mota <aneudy.motacatalino@gmail.com>
2022-05-25 11:06:10 -0400

    Adding selenium and webdriver manager support

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -7,6 +7,7 @@ from command.cas import Cas
 def test_authenticate_text_file():
     test_file = os.path.join(os.getcwd(), 'test_data', 'Aneudy_Resume.txt')
     cas = Cas()
+    cas.login()
     res = cas.authenticate_file(test_file)
     assert res.get('verified') is True
 
@@ -14,5 +15,6 @@ def test_authenticate_text_file():
 def test_authenticate_git_repo():
     test_repo = os.path.join(os.getcwd(), 'test_data', 'working')
     cas = Cas()
+    cas.login()
     res = cas.authenticate_repo(test_repo)
     assert res.get('verified') is True
