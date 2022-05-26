0737af9753112f53ffeeb78ff7ce84fb73ed972b
Aneudy Mota <aneudy.motacatalino@gmail.com>
2022-05-26 18:20:52 -0400

    Removed comment from headless option for browser

--- base/ui_driver.py
+++ base/ui_driver.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 class UIDriver:
     def __init__(self):
         options = Options()
-        #options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("no-sandbox")
         options.add_argument("--disable-extensions")
         self.driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
